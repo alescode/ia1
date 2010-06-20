e7f2a3b0fa74c71363ef9aec0f80c061134e99ba
Alejandro Machado <alemachado02@gmail.com>
2010-06-20 12:20:09 -0430

    Listo para experimentos

--- p1/correr.sh
+++ p1/correr.sh
@@ -2,5 +2,5 @@ ulimit -S -v $((512*1024))
 ulimit -S -t $((60*60))
 for i in ejemplos/*
 do
-    ./dodgson -bfs -prop "$i" > "proba/$i-resultado.out"
+    ./dodgson -bfs -prop "$i" > "proba/`basename $i`-resultado.out"
 done
